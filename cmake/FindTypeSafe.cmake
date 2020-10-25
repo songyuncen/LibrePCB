2041f59cf718492aab0e756e5e1ffebac63dc415
Danilo Bargen <mail@dbrgn.ch>
2020-10-26 00:41:31 +0100

    Use CamelCase for cmake target names

--- cmake/FindTypeSafe.cmake
+++ cmake/FindTypeSafe.cmake
@@ -1,15 +1,15 @@
 set(TYPE_SAFE_SUBMODULE_BASEPATH "${PROJECT_SOURCE_DIR}/libs/type_safe")
 if(EXISTS "${TYPE_SAFE_SUBMODULE_BASEPATH}")
-    message(STATUS "Using vendored type_safe")
+    message(STATUS "Using vendored TypeSafe")
 
     # Include local submodule
     add_subdirectory("${TYPE_SAFE_SUBMODULE_BASEPATH}" build EXCLUDE_FROM_ALL)
 
     # Alias static lib to namespaced variant
-    add_library(type_safe::type_safe ALIAS type_safe)
+    add_library(TypeSafe::TypeSafe ALIAS type_safe)
 
     # Stop here, we're done
     return()
 endif()
 
-message(FATAL_ERROR "Did not find type_safe library submodule, did you clone recursively?")
+message(FATAL_ERROR "Did not find TypeSafe library submodule, did you clone recursively?")
